c0b4bdab30930dccd8d8d9b151bfd2aa961798e3
Juan Esteban Castiblanco <juanestebancastiblanco911@gmail.com>
2026-03-23 20:04:23 -0500

    Add real ML model + bilingual description

--- app/models/salesModel.py
+++ app/models/salesModel.py
@@ -1,8 +1,40 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+
+# Training dataset
+# [advertising, price]
+X = np.array([
+    [100, 10],
+    [200, 9],
+    [300, 8],
+    [400, 7],
+    [500, 6],
+    [600, 5],
+    [700, 4],
+    [800, 3],
+    [900, 2],
+    [1000, 1]
+])
+
+# sales
+y = np.array([
+    120,
+    150,
+    200,
+    260,
+    320,
+    400,
+    480,
+    560,
+    650,
+    740
+])
+
+# Train model
+model = LinearRegression()
+model.fit(X, y)
+
+
 def predict(advertising: float, price: float) -> float:
-    """
-    Simple linear regression model for sales forecasting
-    """
-
-    sales = (5.2 * advertising) - (1.8 * price) + 120
-
-    return round(sales, 2)
+    prediction = model.predict([[advertising, price]])
+    return round(prediction[0], 2)
