eddad142d35f8234339cd5066d6ff1a2f24a19a5
Jean Pierre Gonzalez <jean.gonzalez@gmail.com>
2026-03-24 03:28:19 -0500

    Add customer spending prediction use case and update navigation menu

--- app/__init__.py
+++ app/__init__.py
@@ -6,8 +6,11 @@ def create_app():
     from app.controllers.weatherController import weather_bp
     from app.controllers.regressionController import regression_bp
     from app.controllers.indexController import index_bp
+    from app.controllers.customerController import customer_bp
+
     app.register_blueprint(weather_bp)
     app.register_blueprint(regression_bp)
     app.register_blueprint(index_bp)
+    app.register_blueprint(customer_bp)
 
     return app
---
+++ app/controllers/customerController.py
@@ -0,0 +1,34 @@
+from flask import Blueprint, render_template, request
+
+from app.models.customerModel import (
+    low_customer,
+    medium_customer,
+    high_customer
+)
+
+customer_bp = Blueprint(
+    "customer",
+    __name__
+)
+
+
+@customer_bp.route("/customer")
+def customer():
+
+    case = request.args.get("case")
+
+    example = None
+
+    if case == "low":
+        example = low_customer()
+
+    elif case == "medium":
+        example = medium_customer()
+
+    elif case == "high":
+        example = high_customer()
+
+    return render_template(
+        "customer.html",
+        example=example
+    )
