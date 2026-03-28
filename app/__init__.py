8a0885df8289cd2c479202ed91948006b01468d4
Davidcapera <126677035+Davidcapera@users.noreply.github.com>
2026-03-28 18:22:53 -0500

    solve conflicts

--- app/__init__.py
+++ app/__init__.py
@@ -3,16 +3,16 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
 
-    from app.controllers.weatherController import weather_bp
-    from app.controllers.regressionController import regression_bp
-    from app.controllers.indexController import index_bp
-    from app.controllers.customerController import customer_bp
-    from app.controllers.airController import air_bp 
-    from app.controllers.salesController import sales_bp
+    from app.controllers.controllersUseCases.weatherController import weather_bp
+    from app.controllers.controllerMachineSupervised.regressionController import regression_bp
+   # from app.controllers.indexController import index_bp
+    from app.controllers.controllersUseCases.customerController import customer_bp
+    from app.controllers.controllersUseCases.airController import air_bp 
+    from app.controllers.controllersUseCases.salesController import sales_bp
 
     app.register_blueprint(weather_bp)
     app.register_blueprint(regression_bp)
-    app.register_blueprint(index_bp)
+   # app.register_blueprint(index_bp)
     app.register_blueprint(air_bp) 
     app.register_blueprint(sales_bp)
     app.register_blueprint(customer_bp)
