5548df356b23aa7e07c692b1f192d60f5322001b
Cristian M <criistiian-m@hotmail.es>
2026-03-30 21:03:25 -0500

    feat: SGD Classifier with metrics, ROC curve, dashboard

--- app/models/modelMachineSupervised/sgdClassifierModel.py
+++ app/models/modelMachineSupervised/sgdClassifierModel.py
@@ -1,38 +1,34 @@
+# Dataset source:
+# https://scikit-learn.org/stable/modules/generated/sklearn.datasets.load_iris.html
+# Iris dataset from Scikit-learn (used for classification tasks)
+
 from sklearn.linear_model import SGDClassifier
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score, confusion_matrix, roc_curve, auc
 from sklearn.datasets import load_iris
 import numpy as np
 
-# Dataset real
 dataset = load_iris()
 
 X = dataset.data
 y = dataset.target
 
-# Convertir a binario
 y = (y == 0).astype(int)
 
-# División
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42
 )
 
-# Modelo
 model = SGDClassifier(max_iter=1000, tol=1e-3)
 model.fit(X_train, y_train)
 
-# Predicción
 y_pred = model.predict(X_test)
 
-# Probabilidades para ROC
 y_scores = model.decision_function(X_test)
 
-# ROC REAL
 fpr, tpr, _ = roc_curve(y_test, y_scores)
 roc_auc = auc(fpr, tpr)
 
-# Métricas
 metrics = {
     "accuracy": float(round(accuracy_score(y_test, y_pred), 2)),
     "precision": float(round(precision_score(y_test, y_pred), 2)),
